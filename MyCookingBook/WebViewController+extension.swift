a1f8d866f75f2c70f1c02ebbd86d47022e4cf4d0
alex <alexdok62@gmail.com>
2023-07-12 19:33:20 +0300

    add extension to webView

---
+++ MyCookingBook/WebViewController+extension.swift
@@ -0,0 +1,25 @@
+//
+//  WebViewController+extension.swift
+//  MyCookingBook
+//
+//  Created by алексей ганзицкий on 12.07.2023.
+//
+
+import Foundation
+import WebKit
+
+extension WebViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        showProgressView()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        hideProgressView()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        hideProgressView()
+    }
+}
+
