aeed70559149ef5335ec09c062158d544c72035a
alex <alexdok62@gmail.com>
2023-07-18 14:39:25 +0300

    add ingridiens model

--- MyCookingBook/Ingridients.swift
+++ MyCookingBook/Ingridients.swift
@@ -1,12 +1,33 @@
-//
-//  Ingridients.swift
-//  MyCookingBook
-//
-//  Created by алексей ганзицкий on 11.07.2023.
-//
-
-import Foundation
-
-class Ingridients {
+enum MeasuresOfMeasurement {
+    case weight(Double, String)
+    case volume(Double, String)
+    case amount(Double, String)
     
+    static func defaultStringValue(for measuresOfMeasurement: MeasuresOfMeasurement) -> String {
+        switch measuresOfMeasurement {
+        case .weight:
+            return "kg"
+        case .volume:
+            return "liters"
+        case .amount:
+            return "thing"
+        }
+    }
+    
+    init(weight: Double) {
+        self = .weight(weight, "kg")
+    }
+    
+    init(volume: Double) {
+        self = .volume(volume, "liters")
+    }
+    
+    init(amount: Double) {
+        self = .amount(amount, "thing")
+    }
+}
+
+struct Ingridients {
+    let type: String
+    let measuresOfMeasurement: MeasuresOfMeasurement
 }
