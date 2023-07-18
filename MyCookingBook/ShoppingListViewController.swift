4993416bcc56330b0a9cf205f0a4c27cb217c1ba
alex <alexdok62@gmail.com>
2023-07-18 17:34:06 +0300

    add button to add new item

--- MyCookingBook/ShoppingListViewController.swift
+++ MyCookingBook/ShoppingListViewController.swift
@@ -7,10 +7,10 @@ enum Units {
 }
 
 class ShoppingListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
+
     var shoppingList: [Ingridient] = []
     var checkedItems: [Bool] = [false,false,false,false]
-    
+     var numberOfRows = 0
     let tableView = UITableView()
     
     override func viewDidLoad() {
@@ -20,11 +20,13 @@ class ShoppingListViewController: UIViewController, UITableViewDataSource, UITab
         shoppingList.append(createNewitem(name: "молоко", quantity: 1, units: .liters))
         shoppingList.append(createNewitem(name: "fanta", quantity: 1, units: .things))
 
-        
+        numberOfRows = shoppingList.count
         tableView.frame = view.bounds
         tableView.dataSource = self
         tableView.delegate = self
+        
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(MyCustomCell.self, forCellReuseIdentifier: "MyCustomCell")
         view.addSubview(tableView)
     }
     
@@ -42,20 +44,43 @@ class ShoppingListViewController: UIViewController, UITableViewDataSource, UITab
         return newIngridient
     }
     
+    @objc func addButtonTapped() {
+        // Действие при нажатии кнопки "Добавить"
+        // Увеличиваем количество строк
+        shoppingList.append(Ingridient(type: "test", measuresOfMeasurement: .init(volume: 5)))
+        checkedItems.append(false)
+        numberOfRows += 1
+
+        // Обновляем таблицу
+        tableView.reloadData()
+    }
+    
     // MARK: - UITableViewDataSource
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return shoppingList.count
+        return numberOfRows + 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let item = shoppingList[indexPath.row]
-        let isChecked = checkedItems[indexPath.row]
+        let cell: UITableViewCell
         
-        cell.textLabel?.text = item.type
-        cell.accessoryType = isChecked ? .checkmark : .none
-        
+        if indexPath.row == numberOfRows {
+            // Инициализируем ячейку с кнопкой
+            guard let myCell = tableView.dequeueReusableCell(withIdentifier: "MyCustomCell", for: indexPath) as? MyCustomCell else { return UITableViewCell() }
+            let addButton = UIButton(type: .system)
+            addButton.setTitle("Добавить", for: .normal)
+            addButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+            addButton.frame = myCell.contentView.bounds
+            myCell.contentView.addSubview(addButton)
+            return myCell
+        } else {
+            cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            let item = shoppingList[indexPath.row]
+            let isChecked = checkedItems[indexPath.row]
+            
+            cell.textLabel?.text = item.type
+            cell.accessoryType = isChecked ? .checkmark : .none
+        }
         return cell
     }
     
@@ -64,7 +89,20 @@ class ShoppingListViewController: UIViewController, UITableViewDataSource, UITab
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        checkedItems[indexPath.row] = !checkedItems[indexPath.row]
-        tableView.reloadRows(at: [indexPath], with: .automatic)
+        if indexPath.row != numberOfRows {
+            checkedItems[indexPath.row] = !checkedItems[indexPath.row]
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
+}
+
+class MyCustomCell: UITableViewCell {
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        // Сбросить данные ячейки
+        // Например, сбросить текст и аксессуар ячейки
+        textLabel?.text = nil
+        accessoryType = .none
     }
 }
