1389a21c5a90f8084fc0475bbf33034ec5c477e2
alex <alexdok62@gmail.com>
2023-07-19 11:12:23 +0300

    refactoring

---
+++ MyCookingBook/ShoppingListViewController+extension.swift
@@ -0,0 +1,49 @@
+//
+//  ShoppingListViewController+extension.swift
+//  MyCookingBook
+//
+//  Created by алексей ганзицкий on 19.07.2023.
+//
+
+import UIKit
+
+extension ShoppingListViewController:  UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfRows + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell
+        
+        if indexPath.row == numberOfRows {
+            // Инициализируем ячейку с кнопкой
+            guard let myCell = tableView.dequeueReusableCell(withIdentifier: "MyCustomCell", for: indexPath) as? ShoppingListTableViewCell else {
+                return UITableViewCell()
+            }
+            myCell.buttonToAddNewCell.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+            return myCell
+        } else {
+            cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            let item = shoppingList[indexPath.row]
+            let isChecked = checkedItems[indexPath.row]
+            
+            cell.textLabel?.text = item.type
+            cell.accessoryType = isChecked ? .checkmark : .none
+        }
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.row != numberOfRows {
+            checkedItems[indexPath.row] = !checkedItems[indexPath.row]
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
+}
--- MyCookingBook/ShoppingListViewController.swift
+++ MyCookingBook/ShoppingListViewController.swift
@@ -6,7 +6,7 @@ enum Units {
     case things
 }
 
-class ShoppingListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ShoppingListViewController: UIViewController {
 
     var shoppingList: [Ingridient] = []
     var checkedItems: [Bool] = [false,false,false,false]
@@ -26,7 +26,7 @@ class ShoppingListViewController: UIViewController, UITableViewDataSource, UITab
         tableView.delegate = self
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.register(MyCustomCell.self, forCellReuseIdentifier: "MyCustomCell")
+        tableView.register(ShoppingListTableViewCell.self, forCellReuseIdentifier: "MyCustomCell")
         view.addSubview(tableView)
     }
     
@@ -54,90 +54,6 @@ class ShoppingListViewController: UIViewController, UITableViewDataSource, UITab
         // Обновляем таблицу
         tableView.reloadData()
     }
-    
-    // MARK: - UITableViewDataSource
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return numberOfRows + 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell
-        
-        if indexPath.row == numberOfRows {
-            // Инициализируем ячейку с кнопкой
-            guard let myCell = tableView.dequeueReusableCell(withIdentifier: "MyCustomCell", for: indexPath) as? MyCustomCell else {
-                return UITableViewCell()
-            }
-            myCell.buttonToAddNewCell.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
-            return myCell
-        } else {
-            cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-            let item = shoppingList[indexPath.row]
-            let isChecked = checkedItems[indexPath.row]
-            
-            cell.textLabel?.text = item.type
-            cell.accessoryType = isChecked ? .checkmark : .none
-        }
-        return cell
-    }
-    
-    // MARK: - UITableViewDelegate
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        if indexPath.row != numberOfRows {
-            checkedItems[indexPath.row] = !checkedItems[indexPath.row]
-            tableView.reloadRows(at: [indexPath], with: .automatic)
-        }
-    }
+   
 }
 
-class MyCustomCell: UITableViewCell {
-    
-    var buttonToAddNewCell: UIButton!
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-         super.init(style: style, reuseIdentifier: reuseIdentifier)
-         buttonToAddNewCell = createButton(title: "добавить", color: .green)
-         applyButtonStyle(buttonToAddNewCell)
-         addConstraintsButton()
-     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        textLabel?.text = nil
-        accessoryType = .none
-    }
-    
-     func createButton(title: String, color: UIColor) -> UIButton {
-        let button = UIButton(type: .system)
-        button.setTitle(title, for: .normal)
-        button.backgroundColor = color
-        button.layer.cornerRadius = 10
-         applyButtonStyle(button)
-        return button
-    }
-    
-    private func applyButtonStyle(_ button: UIButton) {
-        button.setTitleColor(.black, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-    }
-    
-     func addConstraintsButton() {
-        contentView.addSubview(buttonToAddNewCell)
-        buttonToAddNewCell.translatesAutoresizingMaskIntoConstraints = false
-      
-        NSLayoutConstraint.activate([
-            buttonToAddNewCell.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            buttonToAddNewCell.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            buttonToAddNewCell.widthAnchor.constraint(equalTo: contentView.widthAnchor),
-            buttonToAddNewCell.heightAnchor.constraint(equalTo: contentView.heightAnchor),
-        ])
-    }
-}
