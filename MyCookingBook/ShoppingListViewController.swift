3e171417b95b2c9cd2a1ed0976ab133969af3114
alex <alexdok62@gmail.com>
2023-07-19 11:38:21 +0300

    add alert controller to shoping list add new item

--- MyCookingBook/ShoppingListViewController.swift
+++ MyCookingBook/ShoppingListViewController.swift
@@ -7,7 +7,7 @@ enum Units {
 }
 
 class ShoppingListViewController: UIViewController {
-
+    var selectedUnit: Units = .kilograms
     var shoppingList: [Ingridient] = []
     var checkedItems: [Bool] = [false,false,false,false]
      var numberOfRows = 0
@@ -50,10 +50,88 @@ class ShoppingListViewController: UIViewController {
         shoppingList.append(Ingridient(type: "test", measuresOfMeasurement: .init(volume: 5)))
         checkedItems.append(false)
         numberOfRows += 1
-
+        createAlertController()
         // Обновляем таблицу
         tableView.reloadData()
     }
    
+    
+      func createAlertController() {
+          let alert = UIAlertController(title: "Add New Item to Shopping List", message: "Add name and quantity", preferredStyle: .alert)
+          
+          // Создаем текстовое поле для ввода названия товара
+          alert.addTextField { textField in
+              textField.placeholder = "Enter item name"
+          }
+          
+          // Создаем текстовое поле для ввода количества товара
+          alert.addTextField { [weak self] textField in
+              textField.placeholder = "Enter quantity"
+              textField.keyboardType = .decimalPad
+              
+              // Добавляем переключатель между единицами измерения
+              let unitsSegmentedControl = UISegmentedControl(items: ["kg", "L", "pcs"])
+              unitsSegmentedControl.selectedSegmentIndex = 0 // Устанавливаем выбранной по умолчанию первую единицу измерения (кг)
+              textField.rightView = unitsSegmentedControl
+              textField.rightViewMode = .always
+              
+              // Обработчик события при изменении значения переключателя
+              unitsSegmentedControl.addTarget(self, action: #selector(self?.unitChanged(_:)), for: .valueChanged)
+          }
+          
+          // Добавляем кнопку "Сохранить"
+          let saveAction = UIAlertAction(title: "Save", style: .default) { [weak self, weak alert] _ in
+              // Получаем введенные значения из текстовых полей
+              if let nameTextField = alert?.textFields?.first, let quantityTextField = alert?.textFields?.last {
+                  let itemName = nameTextField.text ?? ""
+                  let itemQuantity = Int(quantityTextField.text ?? "0") ?? 0
+                  
+                  // Вызываем функцию для сохранения товара с введенными значениями и выбранной единицей измерения
+                  self?.saveItem(name: itemName, quantity: itemQuantity, unit: self?.selectedUnit)
+              }
+          }
+          
+          // Добавляем кнопку "Отмена"
+          let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+          
+          // Добавляем кнопки в UIAlertController
+          alert.addAction(saveAction)
+          alert.addAction(cancelAction)
+          
+          // Показываем UIAlertController на экране
+          present(alert, animated: true, completion: nil)
+      }
+      
+      @objc func unitChanged(_ sender: UISegmentedControl) {
+          // Обработчик события при изменении значения переключателя
+          switch sender.selectedSegmentIndex {
+          case 0:
+              selectedUnit = .kilograms
+          case 1:
+              selectedUnit = .liters
+          case 2:
+              selectedUnit = .things
+          default:
+              break
+          }
+      }
+      
+      func saveItem(name: String, quantity: Int, unit: Units?) {
+          // Ваш код для сохранения товара
+          // Здесь можно выполнить нужные вам действия с введенными значениями и выбранной единицей измерения
+          if let selectedUnit = unit {
+              switch selectedUnit {
+              case .kilograms:
+                  // Добавить обработку для единицы измерения килограмм
+                  print("\(quantity) kg of \(name)")
+              case .liters:
+                  // Добавить обработку для единицы измерения литры
+                  print("\(quantity) L of \(name)")
+              case .things:
+                  // Добавить обработку для единицы измерения штуки
+                  print("\(quantity) pcs of \(name)")
+              }
+          }
+      }
 }
 
