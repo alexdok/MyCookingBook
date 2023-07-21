6cbac87ecbffe73da3b82ea5fa001661591955cb
alex <alexdok62@gmail.com>
2023-07-21 15:32:17 +0300

    add networkMonitor

---
+++ MyCookingBook/managers/NetworkMonitor.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkMonitor.swift
+//  MyCookingBook
+//
+//  Created by алексей ганзицкий on 21.07.2023.
+//
+import Network
+
+class NetworkMonitor {
+  static let shared = NetworkMonitor()
+  var isReachable: Bool { status == .satisfied }
+
+  private let monitor = NWPathMonitor()
+  private var status = NWPath.Status.requiresConnection
+
+  private init() {
+    startMonitoring()
+  }
+
+  func startMonitoring() {
+    monitor.pathUpdateHandler = { [weak self] path in
+      self?.status = path.status
+    }
+    let queue = DispatchQueue(label: "NetworkMonitor")
+    monitor.start(queue: queue)
+  }
+
+  func stopMonitoring() {
+    monitor.cancel()
+  }
+}
